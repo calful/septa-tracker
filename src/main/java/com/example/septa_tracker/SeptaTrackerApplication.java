1ede26e5cfa97810c00b2b11659afd905c24262a
cal fuller <caldouful@gmail.com>
2025-08-07 17:00:29 -0400

    add spring boot maven project skeleton

---
+++ src/main/java/com/example/septa_tracker/SeptaTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.septa_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeptaTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeptaTrackerApplication.class, args);
+	}
+
+}
